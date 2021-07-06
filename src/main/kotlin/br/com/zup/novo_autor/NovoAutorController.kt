73d9a0a807d4adc2044ae33ee8bddef082a0f74f
brenohof <brenohenriqueof@gmail.com>
2021-07-06 18:01:14 -0300

    Validando os dados de entrada de uma requisição

--- src/main/kotlin/br/com/zup/novo_autor/NovoAutorController.kt
+++ src/main/kotlin/br/com/zup/novo_autor/NovoAutorController.kt
@@ -2,12 +2,15 @@ package br.com.zup.novo_autor
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class NovoAutorController {
 
     @Post
-    fun cadastrarAutor(request: NovoAutorRequest) {
+    fun cadastrarAutor(@Valid request: NovoAutorRequest) {
         println(request)
     }
 }
