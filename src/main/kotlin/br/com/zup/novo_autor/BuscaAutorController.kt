3168be09d6b0b355e1412068e2581fbd8abaed75
brenohof <brenohenriqueof@gmail.com>
2021-07-08 00:30:34 -0300

    Métodos que geram queries no Micronaut Data

--- src/main/kotlin/br/com/zup/novo_autor/BuscaAutorController.kt
+++ src/main/kotlin/br/com/zup/novo_autor/BuscaAutorController.kt
@@ -3,14 +3,24 @@ package br.com.zup.novo_autor
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 @Controller("/autores")
 class BuscaAutorController(val repository: AutorRepository) {
 
     @Get
-    fun listarAutores(): HttpResponse<List<DetalhaAutorResponse>> {
-        return repository.findAll()
-            .map { autor -> DetalhaAutorResponse(autor) }
-            .let { autores -> HttpResponse.ok(autores) }
+    fun listarAutores(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+        if (email.isBlank()) {
+            return repository.findAll()
+                .map { autor -> DetalhaAutorResponse(autor) }
+                .let { autores -> HttpResponse.ok(autores) }
+        }
+
+        val possivelAutor = repository.findByEmail(email)
+
+        if (possivelAutor.isEmpty) return HttpResponse.notFound()
+
+        return possivelAutor.get()
+            .let { autor -> HttpResponse.ok(DetalhaAutorResponse(autor)) }
     }
 }
