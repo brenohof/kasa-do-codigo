2fb91e0dbf64d739c807beb72e77f00fb99d604f
brenohof <brenohenriqueof@gmail.com>
2021-07-08 01:32:05 -0300

    Demarcação de transação

--- src/main/kotlin/br/com/zup/novo_autor/AlteraAutorController.kt
+++ src/main/kotlin/br/com/zup/novo_autor/AlteraAutorController.kt
@@ -4,11 +4,13 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Put
+import javax.transaction.Transactional
 
 @Controller("/autores")
 class AlteraAutorController(val autorRepository: AutorRepository) {
 
     @Put("/{id}")
+    @Transactional
     fun atualizar(@PathVariable id: Long, descricao: String): HttpResponse<Any> {
         val optionalAutor = autorRepository.findById(id)
 
